025d8986b52f022435265314e2d74c90acfc7711
r.navabian <r.navabian@NAVABIAN>
2023-04-11 13:16:02 +0430

    Update

--- ProtoTypePatern/Program.cs
+++ ProtoTypePatern/Program.cs
@@ -4,7 +4,45 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var money = new Money(10, "$");
-        var moneyColne =money.Clone();
+        // For 1
+        //var money = new Money(10, "$");
+        //var moneyColne = money.Clone();
+
+        //for Person
+        Person p1 = new Person();
+        p1.Age = 42;
+        p1.BirthDate = Convert.ToDateTime("1984-04-13");
+        p1.Name = "Jack Daniels";
+        p1.IdInfo = new IdInfo(666);
+
+        Person p2 = p1.ShallowCopy();
+        Person p3 = p1.DeepCopy();
+
+        Console.WriteLine("Original values of p1, p2, p3:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values:");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values:");
+        DisplayValues(p3);
+
+        p1.Age = 32;
+        p1.BirthDate = Convert.ToDateTime("1900-01-01");
+        p1.Name = "Frank";
+        p1.IdInfo.IdNumber = 7878;
+        Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+        Console.WriteLine("   p1 instance values: ");
+        DisplayValues(p1);
+        Console.WriteLine("   p2 instance values (reference values have changed):");
+        DisplayValues(p2);
+        Console.WriteLine("   p3 instance values (everything was kept the same):");
+        DisplayValues(p3);
+    }
+
+    public static void DisplayValues(Person p)
+    {
+        Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+            p.Name, p.Age, p.BirthDate);
+        Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
     }
 }
